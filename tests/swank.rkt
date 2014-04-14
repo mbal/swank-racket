9c6c92476471ec2f34476bf619021edbdc1d29ce
mbal <matteo.bana@gmail.com>
2014-04-14 14:25:40 +0200

    tests

--- tests/swank.rkt
+++ tests/swank.rkt
@@ -10,3 +10,13 @@
 (test-equal? "test message serialization"                
              (swank-serialize '(:return (:deeply (:nested (:list 1) 2) 3) 4))
              "00002e(:return (:deeply (:nested (:list 1) 2) 3) 4)\n")
+
+(test-equal? "#lang gets stripped 1"
+             (trim-code-for-eval "#lang racket")
+             "")
+
+(test-equal? "#lang gets stripped 2"
+             (trim-code-for-eval "#lang")
+             "")
+
+
